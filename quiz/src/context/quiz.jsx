153d2881bbde0fa542ed9bfc678b9ab59b315bfc
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2025-08-26 12:24:05 -0300

    Configurando o reducer

--- quiz/src/context/quiz.jsx
+++ quiz/src/context/quiz.jsx
@@ -1,8 +1,28 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import questions from "../data/questions"
+
+const STAGES = ["Start", "Playing", "End"]
+
+const initialState = {
+    gameStage: STAGES[0],
+    questions
+}
+//estado do jogo e ação que pode modificar o estado do jogo
+const quizReducer = (state, action) => {
+    console.log(state, action);
+
+    switch (action.type) {
+        case "CHANGE_STATE":
+            return
+
+        default:
+            return state
+    }
+}
 
 export const QuizContext = createContext()
 
 export const QuizProvider = ({ children }) => {
-    const value = {name: "Quiz"}
+    const value = useReducer(quizReducer, initialState)
     return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>
 }
